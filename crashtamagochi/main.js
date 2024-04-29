f2b25f0778d77d7e9d539c95f76f0ff783f31353
Eric Sanz <160590921+ESP15@users.noreply.github.com>
2024-04-29 10:01:02 +0200

    Add files via upload

---
+++ crashtamagochi/main.js
@@ -0,0 +1,9 @@
+function incrementarProgreso() {
+    var progressBar = document.getElementById("progress-bar");
+    var width = parseInt(progressBar.style.width, 10) || 0;
+    if (width < 100) {
+    width += 10;
+    progressBar.style.width = width + "%";
+    }
+}
+
